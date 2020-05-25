6eca9cb8ccc9b7ba9e00db4f838c50e97b545ea9
Norma Sofia Rosillo <normasrosillo@Normas-MacBook-Pro.local>
2020-05-25 13:39:15 -0700

    Add Event model and add predefined events

--- ema/admin.py
+++ ema/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from ema.models import Event
 
 # Register your models here.
+admin.site.register(Event)
--- ema/models.py
+++ ema/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    begin_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    location = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.name)
